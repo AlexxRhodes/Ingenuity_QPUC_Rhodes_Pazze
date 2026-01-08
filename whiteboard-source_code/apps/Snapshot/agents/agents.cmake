fce9382293b2fd67abba73ae6fe59248223f3aa2
AlexxRhodes <alex.rhodes@univ-tlse3.fr>
2026-01-08 22:37:05 +0100

    Remove obsolete Windows resource files and deployment scripts
    - Deleted Snapshot_version.rc.in and Whiteboard_version.rc.in as they are no longer needed.
    - Removed Windeploy.cmake which handled Qt deployment tasks.
    - Eliminated bundle-qt6.sh script used for packaging Qt applications.
    - Deleted icon.png and various font files from the third-party fonts directory.
    - Removed associated font license documentation.

--- whiteboard-source_code/apps/Snapshot/agents/agents.cmake
+++
@@ -1,8 +0,0 @@
-# File list
-target_sources(${PROJECT_NAME} PRIVATE
-    ${CMAKE_CURRENT_SOURCE_DIR}/agents/snapshot.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/agents/snapshot.h
-)
-
-target_include_directories(${PROJECT_NAME} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/agents)
-
--- whiteboard-source_code/apps/Whiteboard/agents/agents.cmake
+++
@@ -1,8 +0,0 @@
-# File list
-target_sources(${PROJECT_NAME} PRIVATE
-    ${CMAKE_CURRENT_SOURCE_DIR}/agents/whiteboard.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/agents/whiteboard.h
-)
-
-target_include_directories(${PROJECT_NAME} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/agents)
-
